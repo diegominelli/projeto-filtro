2d5a4da9beffc55d50d38cd0d2f80a18fb8b2e1c
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2024-08-25 17:41:19 -0300

    criacao e desenvolvimento do componente filter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ComponentsModule } from './components/components/components.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ComponentsModule } from './components/components.module';
 
 @NgModule({
   declarations: [
--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 import { AngularMaterialModule } from 'src/app/angular-material/angular-material.module';
-import { UserDetailsComponent } from '../user-details/user-details.component';
+
+import { UserDetailsComponent } from './user-details/user-details.component';
+import { FilterComponent } from './filter/filter.component';
 
 @NgModule({
   declarations: [
-    UserDetailsComponent
+    UserDetailsComponent,
+    FilterComponent
   ],
   imports: [
     CommonModule,
+    FormsModule,
     AngularMaterialModule,
   ],
   exports: [
-    UserDetailsComponent
+    UserDetailsComponent,
+    FilterComponent
   ]
 })
 export class ComponentsModule { }
