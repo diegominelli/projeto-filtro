2728ff1c302078895be321dd4c36d661f805cd9f
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2024-08-29 20:25:25 -0300

    armazenando os valores dos filtros

--- src/app/components/filter/filter.component.ts
+++ src/app/components/filter/filter.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IFilterOptions } from 'src/app/interfaces/user/filter-options.interface';
 
 @Component({
   selector: 'app-filter',
@@ -6,7 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./filter.component.scss']
 })
 export class FilterComponent {
-  foods = [
-   { value: 'teste', viewValue: 'Teste' },
- ]
+  filterOptions: IFilterOptions = {
+    name: undefined,
+    startDate: undefined,
+    endDate: undefined,
+    status: undefined
+  };
+
+  statusList = [
+    { description: 'Ativo', value: true},
+    { description: 'Inativo', value: false},
+  ];
+
+  onFilter() {
+    console.log(this.filterOptions);
+    
+  }
 }
